6864cc8a6ff39f95aa15622b145d89ea57fdc127
Anthony <anthony@defenseunicorns.com>
2022-03-09 21:08:14 -0700

    Initial structs section commit

---
+++ structs-methods-interfaces/shapes_test.go
@@ -0,0 +1,27 @@
+package smi
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+
+	t.Run("Perimeter Test", func(t *testing.T) {
+		got := Perimeter(10.0, 10.0)
+		want := 40.0
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
+}
+
+func TestArea(t *testing.T) {
+	t.Run("Area Test", func(t *testing.T) {
+		got := Area(12.0, 6.0)
+		want := 72.0
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+}

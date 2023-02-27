e7ca21eef4a510cef2c853de6ae2e2908ad96d51
Anthony <anthony@defenseunicorns.com>
2023-02-26 19:46:03 -0700

    finish structs, methods and interfaces section

--- structs-methods-interfaces/shapes.go
+++ structs-methods-interfaces/shapes.go
@@ -1,9 +1,37 @@
 package smi
 
-func Perimeter(width, height float64) float64 {
-	return 2 * (width + height)
+import "math"
+
+type Shape interface {
+	Area() float64
 }
 
-func Area(width, height float64) float64 {
-	return width * height
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func (rectangle Rectangle) Area() float64 {
+	return rectangle.Width * rectangle.Height
+}
+
+func (circle Circle) Area() float64 {
+	return math.Pi * circle.Radius * circle.Radius
+}
+
+func (triangle Triangle) Area() float64 {
+	return (triangle.Base * triangle.Height) * 0.5
 }
--- structs-methods-interfaces/shapes_test.go
+++ structs-methods-interfaces/shapes_test.go
@@ -5,7 +5,8 @@ import "testing"
 func TestPerimeter(t *testing.T) {
 
 	t.Run("Perimeter Test", func(t *testing.T) {
-		got := Perimeter(10.0, 10.0)
+		rectangle := Rectangle{10.0, 10.0}
+		got := Perimeter(rectangle)
 		want := 40.0
 
 		if got != want {
@@ -16,13 +17,21 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("Area Test", func(t *testing.T) {
-		got := Area(12.0, 6.0)
-		want := 72.0
 
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{12, 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{12, 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.hasArea {
+			t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
 		}
-	})
-	// To be continued...
+	}
 }
